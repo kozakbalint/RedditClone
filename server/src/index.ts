14cc2519afc67522ef06f7247160f7d065b4e78a
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2022-01-15 20:57:51 +0100

    Mikro-Orm Setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world")
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+    // const post = orm.em.create(Post, { title: "my dasd post" });
+    // await orm.em.persistAndFlush(post);
+};
+
+main().catch((err) => {
+    console.error(err);
+});
