e54394e8a3992e76868f6e8fa04c013b81c4d01b
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2022-01-16 00:23:50 +0100

    graphql setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,30 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
-    // const post = orm.em.create(Post, { title: "my dasd post" });
-    // await orm.em.persistAndFlush(post);
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false,
+        })
+    });
+
+    await apolloServer.start()
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log("server started at http://localhost:4000")
+    })
 };
 
 main().catch((err) => {
