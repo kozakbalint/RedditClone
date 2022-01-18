1cfa7b5ef8e21862bfa9f85915d2ea489fa5885f
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2022-01-18 19:45:46 +0100

    CRUD on Posts & Prettier

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,32 +1,37 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express'
-import { buildSchema } from 'type-graphql'
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
-    const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
-    const app = express();
+  const app = express();
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver],
-            validate: false,
-        })
-    });
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({
+      em: orm.em,
+    }),
+  });
 
-    await apolloServer.start()
-    apolloServer.applyMiddleware({ app })
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
-    app.listen(4000, () => {
-        console.log("server started at http://localhost:4000")
-    })
+  app.listen(4000, () => {
+    console.log("server started at http://localhost:4000");
+  });
 };
 
 main().catch((err) => {
-    console.error(err);
+  console.error(err);
 });
--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -4,14 +4,14 @@ import { MikroORM } from "@mikro-orm/core";
 import path from "path";
 
 export default {
-    migrations: {
-        path: path.join(__dirname, "./migrations"),
-        pattern: /^[\w-]+\d+\.[tj]s$/,
-    },
-    entities: [Post],
-    dbName: "reddit",
-    type: "postgresql",
-    user: "postgres",
-    password: "postgres",
-    debug: !__prod__,
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post],
+  dbName: "reddit",
+  type: "postgresql",
+  user: "postgres",
+  password: "postgres",
+  debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0];
